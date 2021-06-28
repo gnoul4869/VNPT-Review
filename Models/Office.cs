2e56597d14aec8dfc856a80ae05e1106eb3a231c
gnoul_ <ttluong4869@gmail.com>
2021-06-28 18:41:46 +0700

    Add necessary packages
    Added packages:
    - Microsoft.EntityFrameWorkCore.Tools
    - Microsoft.EntityFrameWorkCore.SqlServer
    - Microsoft.VisualStudio.Web.CodeGeneration.Design
    - Oracle.ManagedDataAccess.Core

---
+++ Models/Office.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+#nullable enable
+#nullable disable warnings
+
+namespace VNPT.Models
+{
+    public class OFFICE
+    {
+        [Key]
+        [Display(Name = "Mã phòng ban")]
+        [StringLength(5)]
+        public string ID { get; set; }
+
+        [Required]
+        [Display(Name = "Tên phòng ban")]
+        [StringLength(50)]
+        public string NAME { get; set; }
+
+        [Display(Name = "Ghi chú")]
+        [StringLength(200)]
+        public string? NOTE { get; set; }
+
+        [Display(Name = "Đơn vị cha")]
+        [StringLength(5)]
+        public string FATHERID { get; set; }
+
+        [Display(Name = "Hoạt động")]
+        public bool ACTIVE { get; set; }
+
+        [Display(Name = "Ngày tạo")]
+        public DateTime CREATED_AT { get; set; }
+
+        [Display(Name = "Ngày cập nhật")]
+        public DateTime UPDATED_AT { get; set; }
+
+        public virtual REVIEW REVIEW { get; set; }
+    }
+}
---
+++ Models/Review.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable disable
+
+namespace VNPT.Models
+{
+    public class REVIEW
+    {
+        [Key]
+        [Display(Name = "Mã đánh giá")]
+        public string ID { get; set; }
+
+        [ForeignKey("ID")]
+        [Display(Name = "Mã phòng ban")]
+        public string OFFICE_ID { get; set; }
+
+        [Required]
+        [Display(Name = "Mức đánh giá")]
+        public decimal RATING { get; set; }
+
+        [Required]
+        [Display(Name = "Nội dung đánh giá")]
+        public string CONTENT { get; set; }
+
+        [Display(Name = "Ngày tạo")]
+        public DateTime CREATED_AT { get; set; }
+
+        [Display(Name = "Ngày cập nhật")]
+        public DateTime UPDATED_AT { get; set; }
+
+        public virtual OFFICE OFFICE { get; set; }
+    }
+}
