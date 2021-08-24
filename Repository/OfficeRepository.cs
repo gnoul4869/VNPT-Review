fcfce5c0005bf0d0001112644903dde2c8869ede
gnoul_ <ttluong4869@gmail.com>
2021-08-24 13:09:38 +0700

    Test connection string

--- Repository/OfficeRepository.cs
+++ Repository/OfficeRepository.cs
@@ -29,7 +29,8 @@ namespace VNPT_Review.Repository
                 var pgPort = Environment.GetEnvironmentVariable("PORT");
                 var pgDatabase = Environment.GetEnvironmentVariable("DATABASE");
 
-                var connStr = $"User Id={pgUser}; Password={pgPassword}; Host={pgHost}; Port={pgPort}; Database={pgDatabase}; sslmode=Prefer; Trust Server Certificate=true";
+                // var connStr = $"User Id={pgUser}; Password={pgPassword}; Host={pgHost}; Port={pgPort}; Database={pgDatabase}; sslmode=Prefer; Trust Server Certificate=true";
+                var connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPassword};Database={pgDatabase}";
                 this.db = new NpgsqlConnection(connStr);
             } 
         }
